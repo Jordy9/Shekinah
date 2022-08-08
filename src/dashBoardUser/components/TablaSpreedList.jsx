ac85e0955f8543699f38ecb9f2d8387a61d9ca06
Jordy9 <xilero23@gmail.com>
2022-08-07 20:27:17 -0400

    Filtro del top 10 y organizacion

--- src/dashBoardUser/components/TablaSpreedList.jsx
+++ src/dashBoardUser/components/TablaSpreedList.jsx
@@ -5,12 +5,18 @@ import { TableContent } from './TableContent'
 export const TablaSpreedList = () => {
     const { usuarios } = useSelector(state => state.auth)
 
+    const usuariosTop = () => {
+        const usuariosOrdenados = usuarios?.map(usuarios => usuarios).sort((a, b) => a - b)
+
+        return usuariosOrdenados.slice(0, 10)
+    }
+
     return (
         <>
             {
                 (usuarios)
                     &&
-                    usuarios?.map(usuario => {
+                    usuariosTop()?.map(usuario => {
                         return (
                             <TableContent key = {usuario?.id} {...usuario} />
                         )
